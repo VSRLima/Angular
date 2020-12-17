919f71fb4b8aa3b6790d7bb19d6910bcfd7b010c
VSRLima <vinisrlima_@hotmail.com>
2020-12-17 18:53:37 -0300

    Using a webservice in the form, to get data from the Zip Code of the user

--- aula-forms/src/app/template-form/template-form.component.ts
+++ aula-forms/src/app/template-form/template-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -13,7 +14,7 @@ export class TemplateFormComponent implements OnInit {
     email: null
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +22,78 @@ export class TemplateFormComponent implements OnInit {
   onSubmit(form) {
     console.log(form);
     console.log(this.usuario);
+  }
+
+  verificadorTouchedEValid(campo) {
+    if(!campo.valid && campo.touched) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  consultaCEP(cep, form) {
+    //"cep" somente com dígitos
+    cep = cep.replace(/\D/g,'');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        console.log("validada");
+        this.resetaDadosForm(form);
+        this.http.get(`https://viacep.com.br/ws/${cep}/json`).subscribe(
+          dados =>{
+            this.popularDadosForm(dados, form);
+          } 
+          );
+      } else {
+        //cep é inválido
+        this.resetaDadosForm(form);
+        alert("Por gentileza, digite o CEP corretamente (SOMENTE NÚMEROS)");
+        console.log
+      }
+    }
+  }
+
+  popularDadosForm(dados, form) {
+   /*form.setValue({
+      nome: null,
+      email: null,
+      endereco: {
+        cep: dados.cep,
+        numero: "",
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro ,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })*/
+    form.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        bairro: dados.bairro ,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+    console.log(form);
+  }
+
+  resetaDadosForm(form) {
+    form.form.patchValue({
+      endereco: {
+        cep: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
   }
 
 }
