f9e443e68343114c1cf2b41ba0cd4e321189e78e
VSRLima <vinisrlima_@hotmail.com>
2020-12-22 19:59:27 -0300

    Editing data driven to receive data from webservice

--- aula-forms/src/app/data-form/data-form.component.ts
+++ aula-forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,7 +11,7 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
 
@@ -20,14 +21,90 @@ export class DataFormComponent implements OnInit {
     });*/
 
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      endereco: this.formBuilder.group({
+        cep: [null, Validators.required],
+        numero: [null, Validators.required],
+        complemento: [null],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required]
+      })
     })
+  }
 
+  verificadorTouchedEValid(campo) {
+    if(!this.formulario.get(campo).valid && this.formulario.get(campo).touched) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   onSubmit() {
     console.log(this.formulario);
+    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value)).subscribe( dados => {
+      console.log(dados);
+      //this.resetar();
+    }, (error:any) => alert('erro'));
   }
+
+  resetar() {
+    this.formulario.reset();
+  }
+
+  consultaCEP() {
+    let cep = this.formulario.get('endereco.cep').value;
+    //"cep" somente com dígitos
+    cep = cep.replace(/\D/g,'');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        console.log("validada");
+        this.resetaDadosForm();
+        this.http.get(`https://viacep.com.br/ws/${cep}/json`).subscribe(
+          dados =>{
+            this.popularDadosForm(dados);
+          } 
+          );
+      } else {
+        //cep é inválido
+        this.resetaDadosForm();
+        alert("Por gentileza, digite o CEP corretamente (SOMENTE NÚMEROS)");
+        console.log
+      }
+    }
+  }
+
+  popularDadosForm(dados) {
+    this.formulario.patchValue({
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        bairro: dados.bairro ,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+  }
+
+  resetaDadosForm() {
+    this.formulario.patchValue({
+      endereco: {
+        cep: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
+  }
+   
 
 }
