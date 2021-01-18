b9394d05eca07125c6b7591de6f5f4004c62910d
VSRLima <vinisrlima_@hotmail.com>
2021-01-18 18:35:59 -0300

    Custom input

--- aula-forms/src/app/shared/input-field/input-field.component.ts
+++ aula-forms/src/app/shared/input-field/input-field.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+const INPUT_FIELD_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputFieldComponent),
+  multi: true
+}
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.css']
+  styleUrls: ['./input-field.component.css'],
+  providers: [INPUT_FIELD_VALUE_ACCESSOR]
 })
 export class InputFieldComponent implements  ControlValueAccessor {
 
@@ -13,8 +19,41 @@ export class InputFieldComponent implements  ControlValueAccessor {
   @Input() type = 'text';
   @Input() holder: string;
   @Input() control;
+  @Input() isReadOnly = false;
+
+  private innerValue:any;
+
+  get value() {
+    return this.innerValue;
+  }
+
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCb(v);
+    }
+  }
 
   constructor() { }
+
+  onChangeCb: (_:any) => void = () => {};
+  onTouchedCb: (_:any) => void = () => {};
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCb = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCb = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+   this.isReadOnly = isDisabled;
+  }
 
 
 
